57b58d67a32a1684fc7b8215817492f505c2bc0b
Your Username <you@example.com>
2021-06-08 12:50:33 -0400

    Create boxes for game, button, input

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
-  belongs_to :user
+  belongs_to :user 
+  has_one :score  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
+    has_many :games 
+    has_many :scores, through: :games 
+    validates :username, presence: true 
+
 end
